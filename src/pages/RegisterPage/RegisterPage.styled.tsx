5aa79639feda199a297c552e7d5da0994da86ff4
AlexKat <katsubo79@gmail.com>
2024-07-08 18:20:41 +0300

    phone

--- src/pages/RegisterPage/RegisterPage.styled.tsx
+++ src/pages/RegisterPage/RegisterPage.styled.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Phone = styled.div`
   /* height: calc(100vh - 88px); */
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   background-color: #f8f8f8;
 `;
 
--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -18,21 +18,21 @@ import decorImg from '../../assets/img/vector.svg';
 
 const RegisterPage: React.FC = () => {
   return (
-    <Phone>
-      <Box>
-        <Images src={img} alt="study-picture" />
-        <RegisterForm />
-        <BulletListBox>
-          <ImageBig src={img} alt="study-picture" />
-          <BulletList />
-          <Decorite />
-          {/* <Bgr src={decorImg} alt="decor" /> */}
-          {/* <BoxBack>
+    // <Phone>
+    <Box>
+      <Images src={img} alt="study-picture" />
+      <RegisterForm />
+      <BulletListBox>
+        <ImageBig src={img} alt="study-picture" />
+        <BulletList />
+        <Decorite />
+        {/* <Bgr src={decorImg} alt="decor" /> */}
+        {/* <BoxBack>
           <Triug />
         </BoxBack> */}
-        </BulletListBox>
-      </Box>
-    </Phone>
+      </BulletListBox>
+    </Box>
+    // </Phone>
   );
 };
 
